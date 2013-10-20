619d4d883b6117e99824b324e05a28c33ebaf47b
Russell Folk <rfolk@russellfolk.com>
2013-10-19 23:52:07 -0600

    Unit test for the menu.
    All functions for the menu are completely working. Time to merge this with the
    master branch and begin working on the sockets.

---
+++ test/test_menu.c
@@ -0,0 +1,17 @@
+/**-----------------------------------------------------------------------------
+	* Test program for the menu
+	*
+	* Created: 2013-10-19
+	* Revised:
+	*---------------------------------------------------------------------------*/
+
+#include "menu.h"
+
+int
+main ( void )
+{
+	int menu_return ;
+	menu_return = menu () ;
+	printf ( "Menu returns: %d\n" , menu_return ) ;
+	return 0 ;
+}
